bed93fd00795286fd6b51b705d91ea3877e125d1
WenBin-Zhang <1012370535@qq.com>
2020-06-24 23:52:14 +0800

    导入自定义的navigator组件

--- blog-front/src/App.vue
+++ blog-front/src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <navigator></navigator>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Navigator from './components/Navigator.vue'
+export default {
+  data () {
+    return {
+      //
     }
+  },
+  methods: {
+    //
+  },
+  components: {
+    Navigator
   }
 }
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  // #nav {
+  //   padding: 30px;
+
+  //   a {
+  //     font-weight: bold;
+  //     color: #2c3e50;
+
+  //     &.router-link-exact-active {
+  //       color: #42b983;
+  //     }
+  //   }
+  // }
+
 </style>
