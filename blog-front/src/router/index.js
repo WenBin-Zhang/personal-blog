d783e01d3bd9a86aff00c7e2011c6a5f6de9cc0e
WenBin-Zhang <1012370535@qq.com>
2020-06-25 18:48:54 +0800

    添加originalPush来防止重复触发相同路由导致的浏览器报错

--- blog-front/src/router/index.js
+++ blog-front/src/router/index.js
@@ -4,11 +4,20 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/home',
+    redirect: '/'
   },
   {
     path: '/about',
